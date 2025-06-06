f8109d427ef12f6245b4d9970597b9bea49a8a60
omyagarde <omyagarde@gmail.com>
2025-06-06 15:12:05 +0530

    init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,5 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+function Home() {
+  return <h1>works</h1>
 }
 
-export default function Home() {
-  return <Welcome />;
-}
+export { Home }
